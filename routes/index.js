dd42dd44a47dc3d4ea275c1f36aa782349122c1b
cavis <ryancavis@gmail.com>
2015-09-20 11:51:47 -0400

    Fix another merge conflict

--- routes/index.js
+++ routes/index.js
@@ -7,55 +7,9 @@ var Sitting = require('../models/sitting');
 
 router.get('/', function(req, res, next) {
 
-<<<<<<< HEAD
-	var current_podcasts = [
-		{
-			"id":0,
-			"title": "This American Life",
-			"episode": "Episode Name",
-			"total_listeners": "8",
-			"length": 1800, //30 minutes, 1800 seconds
-			"current_time": 800, //current timestamp in stream
-
-		},
-		{
-			"id":1,
-			"title": "This American Life",
-			"episode": "Episode Name",
-			"total_listeners": "4",
-			"length": 1800, //30 minutes, 1800 seconds
-			"current_time": 1200, //current timestamp in stream
-
-		},
-		{
-			"id":2,
-			"title": "This American Life",
-			"episode": "Episode Name",
-			"total_listeners": "6",
-			"length": 1800, //30 minutes, 1800 seconds
-			"current_time": 200, //current timestamp in stream
-
-		},
-		{
-			"id":3,
-			"title": "This American Life",
-			"episode": "Episode Name",
-			"total_listeners": "12",
-			"length": 1800, //30 minutes, 1800 seconds
-			"current_time": 300, //current timestamp in stream
-
-		}
-	];
-
-  res.render('index', {current_podcasts: current_podcasts, current_user: req.user});
-=======
 	Sitting.find().exec(function(err, data) {
 		res.render('index', {sittings: data, current_user: req.user});
 	});
-
-
-
->>>>>>> 383bd19f0cdc7f6aa229e09f6a50888de43b1c2a
 
 });
 
