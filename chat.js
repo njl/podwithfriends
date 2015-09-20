35ecd2053877162348e29779bc57877ab2b111ee
Ned Jackson Lovely <njl@njl.us>
2015-09-20 11:48:39 -0400

    opentok rough-in

--- chat.js
+++ chat.js
@@ -1,3 +1,23 @@
+var OpenTok = require('opentok'),
+        opentok = new OpenTok(process.env.TOKBOX_API_KEY, process.env.TOKBOX_API_SECRET);
+
+var voice_sessions = {};
+
+function start_voice(name, cb){
+    if(voice_sessions[name]){
+        cb(voice_sessions[name]);
+        return;
+    }
+    opentok.createSession(function(err, session) {
+        if(err) return console.log(err);
+        console.log('session', session);
+        console.log(session.sessionId);
+        voice_sessions[name] = session.sessionId;
+        cb(voice_sessions[name]);
+    });
+
+}
+
 function init(io){
 io.on('connection', function (socket) {
     socket.on('join', function(data){
@@ -7,6 +27,11 @@ io.on('connection', function (socket) {
         socket.name = data.name;
         socket.join(data.room_id);
         socket.emit('joined', {room:data.room_id});
+        start_voice(data.room_id, function(session_id){
+            socket.emit('voice_token', {api_key:process.env.TOKBOX_API_KEY,
+                                        session_id:session_id,
+                                        token: opentok.generateToken(session_id, {data:'name='+data.name})});
+        });
     });
     socket.on('name', function(data){
         socket.name = name;
